424b8b22a1b04e62266055dceb4b229c1e12a6a6
Stephan Kostin <va.lenta0907@gmail.com>
2024-03-04 18:39:00 +0300

    Code Review

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import genDiffJ from './formats/json-form.js';
 import genDiffS from './formats/standart-form.js';
 import getWithFunction from './getWithFunc.js';
 
-const genDiff = (file0, file1, format = 'standart') => {
+const genDiff = (filepath1, filepath2, format = 'standart') => {
   if (format === 'json') {
-    return getWithFunction(file0, file1, genDiffJ);
+    return getWithFunction(filepath1, filepath2, genDiffJ);
   }
   if (format === 'plain') {
-    return getWithFunction(file0, file1, genDiffP);
+    return getWithFunction(filepath1, filepath2, genDiffP);
   }
 
-  return getWithFunction(file0, file1, genDiffS);
+  return getWithFunction(filepath1, filepath2, genDiffS);
 };
 
 export default genDiff;
--- src/readAndParse.js
+++ src/readAndParse.js
@@ -1,28 +1,15 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 
-const findParse = (typeFile) => {
-  if (typeFile === '.json') {
-    return JSON.parse;
-  }
-  if (typeFile === '.yaml' || typeFile === '.yml') {
-    return yaml.load;
-  }
-
-  return undefined;
-};
-
-const readAndParseFile = (files) => {
-  try {
-    const format = path.extname(files);
-    const data = fs.readFileSync(files, 'utf-8');
-    const parse = findParse(format);
-
-    return parse(data);
-  } catch {
-    return undefined;
+const getParsedData = (data, ext) => {
+  switch (ext) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yaml':
+    case '.yml':
+      return yaml.load(data);
+    default:
+      return undefined;
   }
 };
 
-export default readAndParseFile;
+export default getParsedData;
